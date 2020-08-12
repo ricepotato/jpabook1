eb01236d8d93325e87dd896ef60d70a344d36615
Sukjun Sagong <ricepotato40@gmail.com>
2020-08-12 19:30:23 +0900

    요구사항
    Member 엔티티 변경
    RoleType 추가

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,5 @@
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="MEMBER")
@@ -11,6 +12,18 @@ public class Member {
     @Column(name = "NAME")
     private String username;
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public String getId() {
         return id;
@@ -36,12 +49,48 @@ public class Member {
         this.age = age;
     }
 
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Member{");
         sb.append("id='").append(id).append('\'');
         sb.append(", username='").append(username).append('\'');
         sb.append(", age=").append(age);
+        sb.append(", roleType=").append(roleType);
+        sb.append(", createdDate=").append(createdDate);
+        sb.append(", lastModifiedDate=").append(lastModifiedDate);
+        sb.append(", description='").append(description).append('\'');
         sb.append('}');
         return sb.toString();
     }
